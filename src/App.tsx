65bdf8fc2d04e729d29a145bff76e221576210c5
junseokoh-hub <lunulatecat@naver.com>
2022-10-18 22:50:29 +0900

    second toy practice [tour]

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,70 @@
-import React, { useCallback, useState } from "react";
-import data from "./data";
-import List from "./List";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
 
-export interface PeopleProps {
-  id: number;
+export interface ToursTypes {
+  id: string;
   name: string;
-  age: number;
+  info: string;
   image: string;
+  price: string;
 }
 
+const url = "https://course-api.com/react-tours-project";
+
 const App: React.FC = () => {
-  const [people, setPeople] = useState<PeopleProps[]>(data);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState<ToursTypes[]>([]);
 
-  const onClick = useCallback(() => {
-    setPeople([]);
+  const removeTour = (id: string) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
   }, []);
-
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main className="my-20 mx-auto w-[90vw] max-w-2xl">
+        <div className="mb-16 text-center">
+          <h2 className="mb-3 leading-5 capitalize tracking-widest text-3xl font-bold">
+            no tours left
+          </h2>
+          <button
+            className="mt-8 py-1 px-2 text-xl inline-block bg-blue-500 rounded-lg capitalize text-white tracking-widest border-transparent cursor-pointer"
+            onClick={() => fetchTours()}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <section className="py-4 px-8 my-20 w-[90vw] max-w-md bg-gray-400 rounded-xl">
-        <h3 className="mb-5 font-normal">{people.length} birthdays today</h3>
-        <List people={people} />
-        <button
-          className="py-2 px-0 mx-auto mt-8 w-full block text-xl font-bold bg-pink-400 text-white border-transparent capitalize rounded-md tracking-widest outline-1 outline-red-500 cursor-pointer"
-          onClick={onClick}
-        >
-          clear all
-        </button>
-      </section>
+    <main className="my-20 mx-auto w-[90vw] max-w-2xl">
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 };
